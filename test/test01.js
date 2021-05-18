8fbb8d27b9df9c83c18cc8274740fb6c76e2ddb1
G4BR1EL0 <gabrielabarca.91@gmail.com>
2021-05-18 12:21:43 +0200

    tiempo 20 minutos intentos 3

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,16 @@
 function geekshubs(index) {
-    return "";
+    let respuesta = "";
+    for (let i=1; i<=index; i++){
+        if(i%3 && i%5) 
+            respuesta += i+"\n";
+        else if(i%3===0 && i%5!=0)
+            respuesta += "Geeks\n";
+        else if(i%5===0 && i%3!=0)
+            respuesta += "Hubs\n";
+        else if(i%5===0 && i%3===0)
+            respuesta += "GeeksHubs\n";
+    }
+    console.log(respuesta);
+    return respuesta;
 }
 module.exports = geekshubs;
